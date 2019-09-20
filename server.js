2fcc019f9850b79c230f2ff6271df7759f03b7d6
Jackson Oppenheim <jacksonopp@gmail.com>
2019-09-20 17:21:03 -0500

    initial push, got sql and express conected

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const exphbs = require("express-handlebars");
+const connection = require("./sql/sqlConfigs");
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+
+app.set("view engine", "handlebars");
+
+connection.connect(function (err) {
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+
+    console.log("connected as id " + connection.threadId);
+});
+
+app.get("/", (req, res) => {
+    connection.query("select * from burgers", (err, data) => {
+        if (err) {
+            return res.send(500);
+        } else {
+            res.render("index", { item: data });
+        }
+    })
+})
+
+
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function () {
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
+});
