95cf39aa377e6c54ff67e23df324b540a6ffee8c
Jackson Oppenheim <jacksonopp@gmail.com>
2019-09-20 19:13:37 -0500

    added put function, connected to database

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const exphbs = require("express-handlebars");
+const path = require("path");
 const connection = require("./sql/sqlConfigs");
+
 
 const app = express();
 
@@ -39,7 +41,23 @@ app.get("/", (req, res) => {
     })
 })
 
+app.post("/", (req, res) => {
+    console.log(req.body);
+    connection.query("insert into burgers (burger) values (?)", [req.body.burgerName], (err, data) => {
+        if (err) throw err;
+        console.log("success")
+    })
+    // res.redirect("/hello");
+})
 
+app.put("/", (req, res) => {
+    console.log(req.body);
+    connection.query("update burgers set is_eaten=1 where id=?", [req.body.id], (err, data) => {
+        if (err) throw err;
+        console.log("success");
+    })
+    res.send("success");
+})
 // Start our server so that it can begin listening to client requests.
 app.listen(PORT, function () {
     // Log (server-side) when our server has started
